611eccedf6fb12fe026b390ee133f25572511614
jcmonkey87 <fuzhonglong@gmail.com>
2022-12-02 16:34:44 +0800

    update component in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 const App = () => {
-  return <h1>Hello from the app component</h1>
+  return (
+    <div className='container'>
+      <h1>My App</h1>
+      <h2>Hello from the app component</h2>
+      <p>You are going to be a great React Developer!</p>
+    </div>
+  )
 }
 
 export default App;
