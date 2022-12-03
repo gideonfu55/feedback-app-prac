811bc655de721dd9a5bc2152346802022ac7f7f1
jcmonkey87 <fuzhonglong@gmail.com>
2022-12-03 15:41:52 +0800

    add button component and comment input criteria

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react"
+import Button from "./Button"
 import Card from "./shared/Card"
 
 const FeedbackForm = () => {
 
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
+  // For the review submission box input text, you have the following requirements:
+  // - if no text is entered, disable the submit button component (through the setBtnDisabled prop).
+  // - if text is entered but the number of characters is less than 10, disable submit button and set alert msg.
+  // - if the criteria of text entered is > 10 characters, remove any alert message and enable the submit button.
   const handTextChange = e => {
-    console.log(e.target.value)
+    if (text.length <= 1) {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 8) {
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters')
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+    setText(e.target.value)
   }
 
   return (
@@ -17,12 +35,14 @@ const FeedbackForm = () => {
         <div className="input-group">
           <input
             onChange={handTextChange}
-            type='text' 
+            type='text'
             placeholder="Write a review" 
             value={text}
           />
-          <button type='submit' >Send</button>
+          <Button type='submit' isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
