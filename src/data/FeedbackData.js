331f77612e5edabddd68adb3a10f336d7be95a74
jcmonkey87 <fuzhonglong@gmail.com>
2022-12-03 12:42:03 +0800

    Feedback form component added

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -2,27 +2,27 @@ const FeedbackData = [
   {
     id: 1,
     rating: 10,
-    text: "This is the best product you can possibly buy for this price!",
+    text: "This is the best service you can possibly have at these rates!",
   },
   {
     id: 2,
     rating: 9,
-    text: "I switched to using this product from year of using another. I never regretted that decision!",
+    text: "The service from this company is simply excellent!",
   },
   {
     id: 3,
     rating: 8,
-    text: "There are just so many similar types of products on the market, but this is a cut above the rest. Highly recommended!",
+    text: "Every time I experienced an issue, the customer service would always resolve it effectively. It is great!",
   },
   {
     id: 4,
     rating: 7,
-    text: "This product is great, just need the price to be lower!"
+    text: "Good service! Though there can be some improvements with the response time."
   },
   {
     id: 5,
     rating: 8,
-    text: "I'm loving this product!"
+    text: "The service provided to me was great, and I can only say I am one satisfied customer."
   },
 
 ]
