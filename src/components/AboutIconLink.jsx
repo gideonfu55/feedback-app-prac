47b3254564ac41394c3cc3628c620823c2f0888f
jcmonkey87 <fuzhonglong@gmail.com>
2022-12-07 14:11:56 +0800

    edit about page link

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -1,11 +1,17 @@
 import { FaQuestion } from "react-icons/fa"
+import { Link } from 'react-router-dom'
 
 const AboutIconLink = () => {
   return (
     <div className="about-link">
-      <a href='/about'>
+      <Link to={{
+        pathname:'./about',
+        // search:'?sort=name',
+        // hash:'#hello',
+      }}
+      >
         <FaQuestion size={30} />
-      </a>
+      </Link>
     </div>
   )
 }
