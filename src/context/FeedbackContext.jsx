55b96c9ec7d2af349213cbb5c3b19d27cf38acf7
jcmonkey87 <fuzhonglong@gmail.com>
2022-12-10 17:01:40 +0800

    add edit button adn setContext for edit

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -8,21 +8,37 @@ const FeedbackContext = createContext()
 export const FeedbackProvider = ({children}) => {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
+  // Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
 
+  // Delete feedback
   const deleteFeedback = id => {
     if (window.confirm('Are you sure you want to delete this comment?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
+  // Edit feedback
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
+  }
+
   return <FeedbackContext.Provider value={{
     feedback,
     deleteFeedback,
-    addFeedback
+    addFeedback,
+    editFeedback,
   }}>
     {children}
   </FeedbackContext.Provider>
